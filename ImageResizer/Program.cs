1b0be9928fda1776fe7d9bc84b087a0168749513
YOULAN <55483125+h810728@users.noreply.github.com>
2019-09-20 13:31:39 +0800

    Add files via upload

---
+++ ImageResizer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace ImageResizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = Path.Combine(Environment.CurrentDirectory, "images");
+            string destinationPath = Path.Combine(Environment.CurrentDirectory, "output");
+
+            ImageProcess imageProcess = new ImageProcess();
+
+            //先清除原本資料夾得內容
+            imageProcess.Clean(destinationPath);
+
+            //取得效能數值
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            //開始進行圖片
+            imageProcess.ResizeImages(sourcePath, destinationPath, 2.0);
+
+            sw.Stop();
+
+            Console.WriteLine($"花費時間: {sw.ElapsedMilliseconds} ms");
+
+            Console.ReadLine();
+        }
+    }
+}
